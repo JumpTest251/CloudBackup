db4cd6d02ee27a70ea5dfce6483e7cc9a6994453
Jumper251 <a.a.m.2502@googlemail.com>
2021-03-22 14:18:00 +0100

    updated decompress

--- utils/compress.js
+++ utils/compress.js
@@ -1,4 +1,5 @@
 const tar = require('tar');
+const { spawn } = require('child_process');
 
 module.exports.compress = function (directory, output, cwd) {
     return tar.c({
@@ -10,8 +11,21 @@ module.exports.compress = function (directory, output, cwd) {
 }
 
 module.exports.uncompress = function (archive, cwd) {
-    return tar.x({
-        file: archive,
-        cwd
+    /* return tar.x({
+         file: archive,
+         cwd
+     }) */
+
+    const exec = spawn('tar', ['xvzf', archive, '-C', cwd]);
+
+    return new Promise((resolve, reject) => {
+        exec.on('error', err => {
+            return reject(err);
+        })
+
+        exec.on('exit', (code, signal) => {
+            resolve({ code, signal });
+        })
     })
+
 }
